fcef2f58a2774886603d0e74653e1ce7c972a3d0
WeAthFoLD <Cheetah77@qq.com>
2014-10-04 22:35:02 +0800

    w

--- src/main/java/cn/weathfold/client/ClientRunner.java
+++ src/main/java/cn/weathfold/client/ClientRunner.java
@@ -16,7 +16,7 @@ import cn.weathfold.critengine.scene.GUIScene;
  */
 public class ClientRunner {
 	
-	public static final boolean AUTO_QUIT = true;
+	public static boolean AUTO_QUIT;
 	
 	public static class MyScene extends GUIScene {
 		
@@ -27,10 +27,11 @@ public class ClientRunner {
 		}
 		
 		public void frameUpdate() {
-			if(CritEngine.getVirtualTime() - initTime > 1000) {
+			if(AUTO_QUIT && CritEngine.getVirtualTime() - initTime > 1000) {
 				Display.destroy();
 				System.exit(0);
 			}
+			super.frameUpdate();
 		}
 	}
 
@@ -39,6 +40,7 @@ public class ClientRunner {
 	 */
 	public static void main(String[] args) {
 		Display.setTitle("CritEngine Demo");
+		AUTO_QUIT = args.length >= 1 && args[0].equals("test");
 		try {
 			Display.setDisplayMode(new DisplayMode(800, 600));
 		} catch(Exception e) {
